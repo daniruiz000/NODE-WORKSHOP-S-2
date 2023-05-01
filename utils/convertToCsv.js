9173b7722061842c76c5db591f97c7600bd7b75b
Daniel Ruiz <danielruimar@gmail.com>
2023-05-01 14:22:35 +0200

    fix csv function

--- utils/convertToCsv.js
+++ utils/convertToCsv.js
@@ -1,8 +1,8 @@
 const convertJsonToCsv = (jsonData) => {
-  const header = ["name", "price", "marketCap", "created_at"].join(",") + "\n";
+  const header = ["name", "price", "marketCap", "created_at", "_id", "createdAt", "updatedAt"].join(",") + "\n";
 
   // Generamos las filas del CSV
-  const rows = jsonData.map((crypto) => [crypto.name, crypto.price, crypto.marketCap, crypto.created_at].join(",") + "\n").join("");
+  const rows = jsonData.map((crypto) => [crypto.id, crypto.name, crypto.price, crypto.marketCap, crypto.created_at, crypto.createdAt, crypto.updatedAt].join(",") + "\n").join("");
 
   // Unimos la cabecera y las filas en un único string
   const csv = header + rows;
